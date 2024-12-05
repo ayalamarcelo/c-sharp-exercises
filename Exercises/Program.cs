5669061bcd29a88e65822d43c832b7835523778f
Marcelo <marcelorubenayala@outlook.com>
2024-12-05 17:30:04 -0300

    Update Exercises

--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -1,26 +1,43 @@
 ﻿using System.Collections;
 using System.Net.Sockets;
+using System.Reflection;
 
 class Program
 { /* Practicando "Struct" */
 
- /* Struct: Tipo de dato compuesto por otros*/
-    struct tipoPersona {
+    /* Struct: Tipo de dato compuesto por otros*/
+    struct fechaNacimiento
+    {
+        public int dia;
+        public int mes;
+        public int anio;
+    }
+
+    struct tipoPersona
+    {
         public string nombre;
         public char inicial;
-        public int edad;
+        public fechaNacimiento diaDeNacimiento;
         public float nota;
     }
 
-    public static void Main(string[] args){
-       tipoPersona persona;
+    public static void Main(string[] args)
+    {
+        // Declaración de la variable del tipo estructura
+        tipoPersona persona;
+      
+        // Inicialización de los campos
+        persona.nombre = "Juan";
+        persona.inicial = 'J';
+        persona.diaDeNacimiento.dia = 15;
+        persona.diaDeNacimiento.mes = 9;
+        persona.diaDeNacimiento.anio = 1998;
+        persona.nota = 7;
 
-       persona.nombre = "Marcelo";
-       persona.inicial = 'M';
-       persona.edad = 34;
-       persona.nota = 8;
+        // Infrome
+        Console.WriteLine("{0} nació el dia {1}", persona.nombre, persona.diaDeNacimiento.dia);
 
-       Console.WriteLine($"La edad de {persona.nombre} es {persona.edad}");
+
     }
-    
+
 }
